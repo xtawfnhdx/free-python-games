51d07a01ccbd37c6220b81e393b7004e8350273a
xtawfnhdx <zhangweilong.001@163.com>
2021-06-28 22:39:07 +0800

    学习：cannon游戏学习完成

--- freegames_study/ga_cannon.py
+++ freegames_study/ga_cannon.py
@@ -2,8 +2,11 @@ import random
 import turtle
 from freegames import vector
 
+# 定义红球的初始位置
 ball = vector(-200, -200)
+# 定义红球的移动距离
 spend = vector(0, 0)
+# 篮球列表
 targets = []
 
 
@@ -16,16 +19,17 @@ def tap(x, y):
 
 
 def draw():
+    # 清屏，重新绘制
     turtle.clear()
 
     for tar in targets:
         turtle.goto(tar.x, tar.y)
         turtle.dot(20, 'blue')
 
-    if not isside(ball):
+    if isside(ball):
         turtle.goto(ball.x, ball.y)
         turtle.dot(10, 'red')
-
+    # 刷新屏幕，在禁用追踪时使用
     turtle.update()
 
 
@@ -34,11 +38,42 @@ def isside(xy):
 
 
 def move():
-    if random.randrange(40)==0:
-        y=random.randrange(-150.150)
-        ta=vector(200,y)
+    if random.randrange(40) == 0:
+        y = random.randrange(-150, 150)
+        ta = vector(200, y)
         targets.append(ta)
 
     for t in targets:
-        t.x=-0.5
+        t.x -= 0.5
 
+    if isside(ball):
+        spend.y -= 0.35
+        ball.move(spend)
+
+    dupe = targets.copy()
+    targets.clear()
+
+    for t in dupe:
+        # 篮球20+红球10的距离的一半
+        if abs(t - ball) > 15:
+            targets.append(t)
+
+    draw()
+
+    for t in targets:
+        if not isside(t):
+            return
+    turtle.ontimer(move, 50)
+
+
+turtle.setup(420, 420, 370, 0)
+# 隐藏乌龟
+turtle.hideturtle()
+turtle.up()
+# 关闭追踪，关闭轨迹
+turtle.tracer(False)
+# 绑定tap函数到鼠标点击屏幕事件
+turtle.onscreenclick(tap)
+move()
+# 暂停画笔绘制，但绘图窗体不关闭
+turtle.done()
