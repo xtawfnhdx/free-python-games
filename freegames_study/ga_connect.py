9b64afbe14c6907de0b6cbc3991cfcdeb918f457
xtawfnhdx <zhangweilong.001@163.com>
2021-06-28 23:15:39 +0800

    学习：connect

---
+++ freegames_study/ga_connect.py
@@ -0,0 +1,43 @@
+import turtle
+from freegames import vector, line
+
+turns = {'red': 'yellow', 'yellow': 'red'}
+state = {'player': 'yellow', 'rows': [0] * 8}
+
+def grid():
+    turtle.bgcolor('light blue')
+    for x in range(-150, 200, 50):
+        line(x, -200, x, 200)
+
+    for x in range(-175, 200, 50):
+        for y in range(-175, 200, 50):
+            turtle.up()
+            turtle.goto(x, y)
+            turtle.dot(40, 'white')
+
+
+def tap(x, y):
+    player=state['player']
+    rows=state['rows']
+
+    row=int((x+200)//50)
+    count=rows[row]
+
+    x=((x+200)//50)*50-200+25
+    y=count*50-200+25
+
+    turtle.up()
+    turtle.goto(x,y)
+    turtle.dot(40,player)
+    turtle.update()
+
+    rows[row]=count+1
+    state['player']=turns[player]
+
+
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
+grid()
+turtle.onscreenclick(tap)
+turtle.done()
