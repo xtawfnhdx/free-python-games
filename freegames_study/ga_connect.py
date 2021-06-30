0dd80c0868e40a07a28109bffe5ca6b9eb91d016
xtawfnhdx <zhangweilong.001@163.com>
2021-06-30 09:03:15 +0800

    学习：提交一些备注

--- freegames_study/ga_connect.py
+++ freegames_study/ga_connect.py
@@ -2,13 +2,16 @@ import turtle
 from freegames import vector, line
 
 turns = {'red': 'yellow', 'yellow': 'red'}
+#rows:用来存储每一列已经有几个着色
 state = {'player': 'yellow', 'rows': [0] * 8}
+print(state)
 
 def grid():
     turtle.bgcolor('light blue')
     for x in range(-150, 200, 50):
+        #划线
         line(x, -200, x, 200)
-
+    #画圆
     for x in range(-175, 200, 50):
         for y in range(-175, 200, 50):
             turtle.up()
@@ -20,9 +23,11 @@ def tap(x, y):
     player=state['player']
     rows=state['rows']
 
+    #获取当前列已经有几个着色
     row=int((x+200)//50)
     count=rows[row]
 
+    #计算要着色的圆的位置
     x=((x+200)//50)*50-200+25
     y=count*50-200+25
 
